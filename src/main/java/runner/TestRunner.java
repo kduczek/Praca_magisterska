7ae01044e34d6e1d6ad1d74ef0a48a58b98fe5ff
kduczek <duczek.krystian@gmail.com>
2022-12-21 23:19:43 +0100

    PRAC-3 - Created runner class and added necessary dependencies

---
+++ src/main/java/runner/TestRunner.java
@@ -0,0 +1,13 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(tags = "",
+        features = {"src/test/resources/features/Logging.feature"},
+        glue = {"steps"}
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
