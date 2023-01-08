0122208009bcc5d29c72c6622e0870c6ebfe96cf
kduczek <duczek.krystian@gmail.com>
2023-01-08 20:05:21 +0100

    PRAC-6 - Moved steps from one file to other

--- src/main/java/steps/ProductPageSteps.java
+++ src/main/java/steps/ProductPageSteps.java
@@ -5,11 +5,9 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.Assert;
 import pages.ProductPage;
-import pages.TopMenuPage;
 
 public class ProductPageSteps {
     ProductPage productPage = new ProductPage();
-    TopMenuPage topMenuPage = new TopMenuPage();
 
     @When("user clicks on {string} size")
     public void pickSize(String size) {
@@ -42,17 +40,6 @@ public class ProductPageSteps {
         productPage.waitForPageToLoad();
         productPage.waitForElementIsVisible(productPage.getProductPageMessagePrompt(), 5, 500);
         Assert.assertEquals(productPage.getText(productPage.getProductPageMessagePrompt()), message, "Wrong message");
-    }
-
-    @Then("number of products in cart should be equal to {int}")
-    public void verifyNumberOfProductsInCart(int numberOfProducts) {
-        Assert.assertEquals(topMenuPage.getText(topMenuPage.getShoppingCartCounter()), String.valueOf(numberOfProducts), "Wrong number of products in cart");
-    }
-
-    @Then("shopping cart should be empty")
-    public void verifyIfCartIsEmpty() {
-        topMenuPage.waitForPageToLoad();
-        Assert.assertTrue(topMenuPage.isElementVisible(topMenuPage.getEmptyCartMessage()));
     }
 
     @Then("validation message \"This is a required field.\" should be visible")
@@ -98,25 +85,5 @@ public class ProductPageSteps {
     @When("user clicks Submit Review button")
     public void clickSubmitReviewButton() {
         productPage.clickElement(productPage.getSubmitReviewButton());
-    }
-
-    @When("user clicks shopping cart icon")
-    public void clickShoppingCartIcon() {
-        topMenuPage.clickElement(topMenuPage.getShoppingCartIcon());
-    }
-
-    @When("user clicks delete icon in shopping cart")
-    public void clickDeleteIconInCart() {
-        topMenuPage.clickElement(topMenuPage.getShoppingCartDeleteIcon());
-    }
-
-    @When("user allows deleting item from cart")
-    public void allowToDeleteFromCart() {
-        topMenuPage.clickElement(topMenuPage.getAllowDeletingFromCart());
-    }
-
-    @When("user declines deleting item from cart")
-    public void declineToDeleteFromCart() {
-        topMenuPage.clickElement(topMenuPage.getCancelDeletingFromCart());
     }
 }
--- src/main/java/steps/ShoppingCartSteps.java
+++ src/main/java/steps/ShoppingCartSteps.java
@@ -1,7 +1,44 @@
 package steps;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
 import pages.ShoppingCartPage;
+import pages.TopMenuPage;
 
 public class ShoppingCartSteps {
     ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
+    TopMenuPage topMenuPage = new TopMenuPage();
+
+    @When("user clicks shopping cart icon")
+    public void clickShoppingCartIcon() {
+        topMenuPage.clickElement(topMenuPage.getShoppingCartIcon());
+    }
+
+    @When("user clicks delete icon in shopping cart")
+    public void clickDeleteIconInCart() {
+        topMenuPage.clickElement(topMenuPage.getShoppingCartDeleteIcon());
+    }
+
+    @When("user allows deleting item from cart")
+    public void allowToDeleteFromCart() {
+        topMenuPage.clickElement(topMenuPage.getAllowDeletingFromCart());
+    }
+
+    @When("user declines deleting item from cart")
+    public void declineToDeleteFromCart() {
+        topMenuPage.clickElement(topMenuPage.getCancelDeletingFromCart());
+    }
+
+    @Then("number of products in cart should be equal to {int}")
+    public void verifyNumberOfProductsInCart(int numberOfProducts) {
+        Assert.assertEquals(topMenuPage.getText(topMenuPage.getShoppingCartCounter()), String.valueOf(numberOfProducts), "Wrong number of products in cart");
+    }
+
+    @Then("shopping cart should be empty")
+    public void verifyIfCartIsEmpty() {
+        topMenuPage.waitForPageToLoad();
+        Assert.assertTrue(topMenuPage.isElementVisible(topMenuPage.getEmptyCartMessage()));
+    }
+
 }
