61a81c2608025f22e96d424ff6e4fa27e7e49b8e
kduczek <duczek.krystian@gmail.com>
2023-01-28 11:58:11 +0100

    PRAC-6 - Updated tag in runner class

--- src/main/java/runners/TestRunner.java
+++ src/main/java/runners/TestRunner.java
@@ -3,7 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "@krystian",
+@CucumberOptions(tags = "",
         features = {"src/test/resources/features"},
         glue = {"steps"}
 )
